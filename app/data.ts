ae427d2489eac0e5864cbb1b61d3440feafdcd9d
Daniyal Master <daniyalmaster693@gmail.com>
2025-05-30 16:39:57 -0400

    Initial commit

---
+++ app/data.ts
@@ -0,0 +1,76 @@
+type Project = {
+  name: string
+  description: string
+  link: string
+  image: string
+  id: string
+}
+
+type BlogPost = {
+  title: string
+  description: string
+  link: string
+  uid: string
+}
+
+export const PROJECTS: Project[] = [
+  {
+    name: 'SuperCorners',
+    description: "Make Your Mac's Corners Useful Again",
+    link: 'https://github.com/daniyalmaster693/SuperCorners',
+    image: '/SuperCorners.png',
+    id: 'project1',
+  },
+  {
+    name: 'MenuScores',
+    description: 'Live Scores. Right From Your Menubar',
+    link: 'https://github.com/daniyalmaster693/MenuScores',
+    image: '/MenuScores.png',
+    id: 'project2',
+  },
+  {
+    name: 'Sportssync',
+    description: 'View sports scores, news, and standings in Raycast',
+    link: 'https://github.com/daniyalmaster693/sportssync',
+    image: '/Sportssync-Project-Page.png',
+    id: 'project2',
+  },
+]
+
+export const BLOG_POSTS: BlogPost[] = [
+  {
+    title: 'Sportssync Developer Blog #2',
+    description: 'Major Features and New Commands',
+    link: '/blog/sportssync-developer-blog-2',
+    uid: 'blog-1',
+  },
+  {
+    title: 'Sportssync Developer Blog #1',
+    description: 'New Features and Visual Updates',
+    link: '/blog/sportssync-developer-blog-1',
+    uid: 'blog-2',
+  },
+  {
+    title: 'My Raycast Setup',
+    description:
+      'How I customized Raycast to speed up my workflow and stay focused.',
+    link: '/blog/my-raycast-setup',
+    uid: 'blog-3',
+  },
+  {
+    title: 'Using an Android Phone with a Mac',
+    description:
+      'Tips, tools, and tricks for making Android and macOS play nicely together.',
+    link: '/blog/using-an-android-phone-with-a-mac',
+    uid: 'blog-4',
+  },
+  {
+    title: 'My Mac Setup',
+    description:
+      'A look into my development environment, tools I use, and how it all fits together.',
+    link: '/blog/my-mac-setup',
+    uid: 'blog-5',
+  },
+]
+
+export const EMAIL = 'daniyalmaster693@gmail.com'
---
+++ app/header.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { TextEffect } from '@/components/ui/text-effect'
+import Link from 'next/link'
+
+export function Header() {
+  return (
+    <header className="mb-8 flex items-center justify-between">
+      <Link href="/" className="font-medium text-black dark:text-white">
+        <div className="flex items-center space-x-4">
+          <img
+            src="/Daniyal.jpg"
+            alt="Daniyal"
+            className="mr-5 h-14 w-14 rounded-full object-cover"
+          />
+          <div>
+            Daniyal Master
+            <TextEffect
+              as="p"
+              preset="fade"
+              per="char"
+              className="text-zinc-600 dark:text-zinc-500"
+              delay={0.5}
+            >
+              Software Engineer
+            </TextEffect>
+          </div>
+        </div>
+      </Link>
+    </header>
+  )
+}
