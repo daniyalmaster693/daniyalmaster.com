91dbe9d8069c1bb79e5969aadae425f226f7967f
Daniyal Master <daniyalmaster693@gmail.com>
2025-05-30 20:01:15 -0400

    Updated Images

--- app/page.tsx
+++ app/page.tsx
@@ -58,8 +58,6 @@ export default function Personal() {
                 <img
                   src={project.image}
                   alt={project.name}
-                  loading="eager"
-                  fetchPriority="high"
                   className="aspect-video w-full rounded-2xl object-cover object-center"
                 />
               </div>
