559dd532700e6ff676f91bad192c93544e5764f7
Daniyal Master <daniyalmaster693@gmail.com>
2026-08-01 13:21:49 -0400

    Updated footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -71,7 +71,7 @@ export function Footer() {
     <footer className="mt-24 border-t border-zinc-100 px-0 py-4 dark:border-zinc-800">
       <div className="flex items-center justify-between">
         <TextLoop className="text-xs text-zinc-400">
-          <span>© 2025 • Daniyal Master.</span>
+          <span>© 2026 • Daniyal Master.</span>
           <span>Credits: Julien Thibeaut</span>
         </TextLoop>
         <div className="text-xs text-zinc-400">
